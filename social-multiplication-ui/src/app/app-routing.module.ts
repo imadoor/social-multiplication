ccb46cbcd753e2e3f2658c5755b49efc86019b05
Amador Ollarzabal <amador.ollarzabal@outlook.com>
2019-09-15 13:03:37 -0500

    added routing to the project as well as some components to route to

--- social-multiplication-ui/src/app/app-routing.module.ts
+++ social-multiplication-ui/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultPageComponent } from './default-page/default-page.component';
+import { ChallengeQuestionComponent } from './challenge-question/challenge-question.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component: DefaultPageComponent},
+  {path:'challengequestion', component:ChallengeQuestionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- social-multiplication-ui/src/app/app.module.ts
+++ social-multiplication-ui/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChallengeQuestionComponent } from './challenge-question/challenge-question.component';
+import { DefaultPageComponent } from './default-page/default-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChallengeQuestionComponent,
+    DefaultPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
